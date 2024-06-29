294f6ad6537d494479ba21cddd9112e07d95fa18
Reece <86894755+consolekid@users.noreply.github.com>
2024-06-29 11:24:10 +0100

    Changeable keys in data dictionary and random email domains

--- phishkiller.py
+++ phishkiller.py
@@ -10,9 +10,14 @@ names = ["alice", "bob", "charlie", "dave", "eve" "fred", "george",
           "yusuf", "zachary",
           ]
 
+#list of domains for email addresses
+domains = [
+    "@gmail.com","@yahoo.com","@outlook.com","@hotmail.com","@icloud.com"
+]
+
 def generate_random_email():
-    name = random.choice(names)
-    domain = "@gmail.com"  # You can change this domain
+    name = random.choice(names) #selects random name from list
+    domain = random.choice(domains)  # selects random domain from list
     return name + str(random.randint(1, 100)) + domain
 
 def generate_random_password():
@@ -20,25 +25,30 @@ def generate_random_password():
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(8))
 
-def send_posts(url):
+def send_posts(url, email_key, password_key):
     while True:
         email = generate_random_email()
         password = generate_random_password()
         data = {
-            "a": email,
-            "az": password
+            email_key: email,
+            password_key: password
         }
         response = requests.post(url, data=data)
         print(f"Email: {email}, Password: {password}, Status Code: {response.status_code}")
 
 def main():
+
     # Ask user for URL to flood
     url = input("Enter the URL of the target you want to flood: ")
+    #asks user for the name of the key that corresponds to the email, different phishing links will use different keys
+    email_key = input("Enter key for email: ")
+    #asks user for the name of the key that corresponds to the passwords
+    password_key = input("Enter key for password: ")
 
     threads = []
 
     for i in range(50):
-        t = threading.Thread(target=send_posts, args=(url,))
+        t = threading.Thread(target=send_posts, args=(url, email_key, password_key))
         t.daemon = True
         threads.append(t)
 
